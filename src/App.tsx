10df9bae2ac0cf7d720b868981deb26da460f640
Max Wood <bridges.wood@gmail.com>
2021-12-26 13:45:47 +0000

    Add demo map view

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import MapView from './components/MapView'
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={{fontSize: 70, fontWeight: 'bold', textAlign: 'center'}}>Ultra Fast Refresh</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView/>
     </View>
   );
 }
@@ -14,9 +12,10 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
 
---
+++ src/components/MapView.tsx
@@ -0,0 +1,46 @@
+import * as Location from 'expo-location'
+import React, { useEffect, useState } from "react"
+import { StyleSheet } from "react-native"
+import Map from 'react-native-maps'
+
+const MapView = () => {
+  const [location, setLocation] = useState<Location.LocationObject | null>(null)
+  
+
+  useEffect(() => {
+    (async () => {
+      let {status} = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        console.error('Failed to get permissions.')
+        return
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location)
+    })()
+  }, [])
+
+  return (
+    <React.Fragment>
+      {location ? <Map
+      style={styles.map}
+        region={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1
+        }}
+        showsUserLocation
+        showsCompass
+      /> : null}
+    </React.Fragment>
+  )
+}
+
+const styles = StyleSheet.create({
+map: {
+  ...StyleSheet.absoluteFillObject
+}
+})
+
+export default MapView
