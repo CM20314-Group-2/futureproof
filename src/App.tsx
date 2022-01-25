b5b3f5aca650a7437c51f1be2992b55c89eace9f
Max Wood <bridges.wood@gmail.com>
2022-01-25 14:41:59 +0000

    Remove spaces and add comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,11 @@ import { StyleSheet, View } from 'react-native'
 
 // Initialise Apollo Client
 const client = new ApolloClient({
-  uri: 'https://api.spacex.land/graphql/',
-  cache: new InMemoryCache()
+  uri: 'https://api.spacex.land/graphql/', // Server URL (must be absolute)
+  cache: new InMemoryCache() // Cache
 })
 
 const App = () => {
-  
-
   return (
     <ApolloProvider client={client}>
       <View style={styles.container}>
