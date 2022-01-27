0d46f54a5f2c79d3359072d0b1cd8334e875a174
Max Wood <bridges.wood@gmail.com>
2022-01-27 14:57:36 +0000

    Fix linter errors and update .gitignore

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,10 +1,10 @@
-import { printSchemaWithDirectives } from "@graphql-tools/utils"
-import { GraphQLSchema } from "graphql"
+import { printSchemaWithDirectives } from '@graphql-tools/utils'
+import { GraphQLSchema } from 'graphql'
 
 const print = (schema: string) => `
   import { gql } from "@apollo/client/core"
   export const typeDefs = gql\`${schema}\`;
-`;
+`
 
 export const plugin = (schema: GraphQLSchema) =>
-  print(printSchemaWithDirectives(schema));
+  print(printSchemaWithDirectives(schema))
