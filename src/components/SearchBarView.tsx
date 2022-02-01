d07ca1ccedb2005b9d4cf2aebcdda52d80dcfbb8
LaurenceBuist <laurence.buist@live.co.uk>
2022-02-01 20:46:37 +0000

    Co-authored-by: Max Wood <bridges-wood@users.noreply.github.com>

--- src/components/SearchBarView.tsx
+++ src/components/SearchBarView.tsx
@@ -1,9 +1,43 @@
 import * as React from 'react';
 import { SafeAreaView, View, StyleSheet} from "react-native"
 import { Icon, Input } from 'react-native-elements';
+import { gql, useLazyQuery} from '@apollo/client'
+import * as GetTypes from '../typings/src/ts/types'
+
+// GraphQL fragment
+const COMPANY_TILE_DATA = gql` {
+    fragment CompanyTile on Business{
+        __typename
+        id
+        name
+        profilePicture
+        sustainabilityScore
+        customerScore
+    }
+}
+`
+;
+
 
 const SearchBar = () => {
     const [value, onChangeText] = React.useState('');
+    const [executeSearch, {loading, error, data}] = useLazyQuery(
+        COMPANY_TILE_DATA
+      );
+
+    // Debounce query
+    React.useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+          executeSearch({
+              variables: {String: value}
+          })
+          console.log(data)
+        }, 400)
+      
+        return () => clearTimeout(delayDebounceFn)
+      }, [value])
+
+    
 
     const updateSearch = (text : string) => {
         onChangeText(text);
@@ -64,3 +98,22 @@ const styles = StyleSheet.create({
 export default SearchBar;
 
 
+/*
+
+// Query 
+//      (hasMore field indicates whether there are additional launches beyond the list returned by the server.)
+//      (cursor field indicates the client's current position within the list of launches. We can execute the query again and provide our most recent cursor as the value of the $after variable to fetch the next set of launches in the list.)
+export const GET_LAUNCHES = gql`
+  query GetLaunchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore 
+      launches {
+        ...LaunchTile
+      }
+    }
+  }
+  ${COMPANY_TILE_DATA}
+`;
+
+*/
