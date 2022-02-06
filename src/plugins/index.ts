7d96efaa6a2370819ec4ea46a5e8d7c453e45205
Max Wood <bridges.wood@gmail.com>
2022-02-06 20:58:36 +0000

    Update codegen for new schema definition
    - Replace gql definition function with string export

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -2,8 +2,7 @@ import { printSchemaWithDirectives } from '@graphql-tools/utils'
 import { GraphQLSchema } from 'graphql'
 
 const print = (schema: string) => `
-  import { gql } from "@apollo/client/core"
-  export const typeDefs = gql\`${schema}\`;
+  export const typeDefs = \`${schema}\`;
 `
 
 export const plugin = (schema: GraphQLSchema) =>
