b505981116b14494386c9a256655520a4c49830e
choobs1 <57880175+choobs1@users.noreply.github.com>
2022-02-07 11:35:32 +0000

    Adds prisma client and sets up resolvers for Data Lists

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { typeDefs } from '@typings/apollo'
 import { ApolloServer } from 'apollo-server'
 import {Business} from '@typings/types'
+import { context, Context } from './context'
 
 // Simple Data for resolver
 const businesses : Pick<Business, 'name' | 'sustainabilityScore' | 'customerScore' | 'profilePicture' | 'profileText'>[] = [
@@ -15,16 +16,26 @@ const businesses : Pick<Business, 'name' | 'sustainabilityScore' | 'customerScor
 
 const resolvers = {
   Query: {
-    users: () =>{
-      return
+    users: (_parent, _args, context:Context) => {
+      return context.prisma.user.findMany()
     },
-    businesses: () => businesses,
-
+    businesses: (_parent, _args, context:Context) => {
+      return context.prisma.business.findMany();
+    },
+    comments: (_parent, _args, context:Context) => {
+      return context.prisma.comment.findMany();
+    },
+    locations: (_parent, _args, context:Context) => {
+      return context.prisma.location.findMany();
+    },
+    reviews: (_parent, _args, context:Context) => {
+      return context.prisma.userReview.findMany();
+    },
   },
 }
 
 
-const server = new ApolloServer({typeDefs, resolvers})
+const server = new ApolloServer({typeDefs, resolvers, context: context})
 
 server.listen().then(({url}) => {
   console.log(`🚀 Server ready at ${url}`)
