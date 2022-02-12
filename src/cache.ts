9afd457bfe5f460ddf9d90aa08d5ea7f86f11d0f
Max Wood <bridges.wood@gmail.com>
2022-02-12 14:09:05 +0000

    Move frontend files

--- src/cache.ts
+++
@@ -1,7 +0,0 @@
-import {InMemoryCache, makeVar} from '@apollo/client'
-import {SortOptions} from '@typings/Search'
-
-const cache = new InMemoryCache()
-export const sortOption = makeVar<SortOptions>('name_asc')
-
-export default cache
