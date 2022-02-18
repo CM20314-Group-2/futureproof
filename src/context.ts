2f14cc9a432623c04e79b694ade497581f7e13aa
wumpyjumps <alexj.english2002@gmail.com>
2022-02-18 13:36:41 +0000

    real commit

--- src/context.ts
+++ src/context.ts
@@ -1,11 +1,6 @@
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
 export interface Context {
-  prisma: PrismaClient
 }
 
 export const context: Context = {
-  prisma: prisma,
+  //prisma: prisma,
 }
