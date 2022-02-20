dd4ce3d2dee074b977c36331644357861a6780ba
Max Wood <bridges.wood@gmail.com>
2022-02-20 13:44:00 +0000

    Fix hooks

--- src/components/BusinessView.tsx
+++ src/components/BusinessView.tsx
@@ -4,6 +4,7 @@ import TitleView from '@components/TitleView'
 import { BusinessType, DisplayableBusiness } from '@futureproof/typings'
 import React from 'react'
 import { SafeAreaView, ScrollView, StyleSheet } from 'react-native'
+import ImagesCarousel from './ImagesCarousel'
 
 // TODO - Update to fetch graphQL
 const ExampleBusiness : DisplayableBusiness =  {
@@ -31,7 +32,7 @@ const BusinessView = () => {
           customerScore={ExampleBusiness.customerScore}
           sustainabilityScore={ExampleBusiness.sustainabilityScore}
         />
-        {/* <ImagesCarousel/> */}
+        <ImagesCarousel/>
       </ScrollView>
     </SafeAreaView>
   )
