592e3b9a92460adf63f6a835ce1da6ed64466075
Matthew5002 <matthew.holmes5002@gmail.com>
2022-03-21 23:16:10 +0000

    Fixed test errors

--- packages/client/src/components/ratings/CircularRatingIndicator.tsx
+++ packages/client/src/components/ratings/CircularRatingIndicator.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import Svg, { Circle } from 'react-native-svg'
 import { ratingToColour } from '@components/search/RatingCapsule'
+import ErrorBoundary from '@components/ErrorBoundary'
 
 const BACKGROUND_SECONDARY_COLOUR = '#FAF9F9'
 
