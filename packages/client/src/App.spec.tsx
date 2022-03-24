973fd708fb23fb090aca12e5ed014892c04b1e43
cam-arch <40884189+cam-arch@users.noreply.github.com>
2022-03-24 21:12:36 +0000

    Fixed feed screen test

--- packages/client/src/App.spec.tsx
+++ packages/client/src/App.spec.tsx
@@ -1,14 +1,35 @@
-import { FeedScreen } from 'App'
 import { render, fireEvent } from '@testing-library/react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import AppNavigator from './App'
 
+it('matches snapshot', () => {
 
+  const component = (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  )
+
+  const { toJSON } = render(component)
+
+  expect(toJSON()).toMatchSnapshot()
+})
 
 it('opens account page when account profile icon is clicked', async () => {
-  const pushMock = jest.fn()
-  const { getByTestId } = render(<FeedScreen navigation={{ push: pushMock }} />)
+  
+  const component = (
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  )
+
+  const { getByTestId, findByText } = render(component)
 
   fireEvent.press(getByTestId('AccountView'))
+  const newHeader = await findByText('Account Settings')
+  const newBody = await findByText('Name')
 
-  expect(pushMock).toBeCalledWith('App')
+  expect(newHeader).toBeTruthy()
+  expect(newBody).toBeTruthy()
 })
