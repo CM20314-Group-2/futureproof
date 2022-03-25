c212c89231ebbcbc10da0c9400713e1665b77b0b
Max Wood <bridges.wood@gmail.com>
2022-03-25 14:37:41 +0000

    Add default fields

--- packages/server/src/resolvers/index.ts
+++ packages/server/src/resolvers/index.ts
@@ -1,9 +1,11 @@
-import businessResolver from '@resolvers/business'
+import businessResolver, { defaultBusinessFields } from '@resolvers/business'
 import commentResolver from '@resolvers/comment'
 import locationResolver from '@resolvers/location'
 import reviewResolver from '@resolvers/review'
 import userResolver from '@resolvers/user'
 import { merge } from 'lodash'
+
+export const defaultFields = merge(defaultBusinessFields)
 
 export default merge(
   businessResolver,
